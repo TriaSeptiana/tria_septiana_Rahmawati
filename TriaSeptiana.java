39a944d52155e5005f3ce27a9411ac2e66b50006
TriaSeptiana <64520178+TriaSeptiana@users.noreply.github.com>
2020-09-09 11:53:41 +0700

    Add files via upload

---
+++ TriaSeptiana.java
@@ -0,0 +1,11 @@
+package TriaSeptiana;
+
+public class TriaSeptiana {
+    public static void main(String[] args) {
+        String huruf="ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+        System.out.println(""+ huruf.charAt(19) + huruf.charAt(17) + huruf.charAt(8) +  huruf.charAt(0) + huruf.charAt(18) + huruf.charAt(4) + huruf.charAt(15) + huruf.charAt(19) + huruf.charAt(8) + huruf.charAt(0) + huruf.charAt(13) + huruf.charAt(0) + huruf.charAt(17) + huruf.charAt(0) + huruf.charAt(7)+ huruf.charAt(12)+ huruf.charAt(0)+ huruf.charAt(22)+ huruf.charAt(0)+ huruf.charAt(19)+ huruf.charAt(8));
+
+    }
+
+}
